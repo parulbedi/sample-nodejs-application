eb95ee0c894e818441239ce783923cefe946561a
Parul Bedi <76744313+parulbedi@users.noreply.github.com>
2023-06-02 17:01:44 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,25 +1,25 @@
-// 'use strict';
+'use strict';
 
-// const express = require('express');
+const express = require('express');
 
-// // Constants
-// const PORT = 3000;
-// const HOST = '0.0.0.0';
+// Constants
+const PORT = 3000;
+const HOST = '0.0.0.0';
 
-// // App
-// const app = express();
-// app.get('/', (req, res) => {
-//   res.send('Hello Node.js Sample!\n');
-// });
+// App
+const app = express();
+app.get('/', (req, res) => {
+  res.send('Hello Node.js Sample!\n');
+});
 
-// const port = process.env.PORT||PORT;
-// app.listen(port);
-// console.log(`Running on http://${HOST}:${PORT}`);
+const port = process.env.PORT||PORT;
+app.listen(port);
+console.log(`Running on http://${HOST}:${PORT}`);
 
-var http = require('http');
+// var http = require('http');
 
-//create a server object:
-http.createServer(function (req, res) {
-  res.write('Hello Node.js'); //write a response to the client
-  res.end(); //end the response
-}).listen(80); //the server object listens on port 80
+// //create a server object:
+// http.createServer(function (req, res) {
+//   res.write('Hello Node.js'); //write a response to the client
+//   res.end(); //end the response
+// }).listen(80); //the server object listens on port 80
